ad34ef949b23900d34f84f96f102eb6a507abebf
BattousaiW <Brandonwee08@gmail.com>
2023-02-22 13:11:29 +0200

    variables and if else

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c, d, x, y;
+a = b = c = d = 48;
+while (d < 58)
+{
+c = 48;
+while (c < 58)
+{
+b = 48;
+while (b < 58)
+{
+a = 48;
+while (a < 58)
+{
+x = (d * 10) + c;
+y = (b * 10) + a;
+if (x < y)
+{
+putchar(d);
+putchar(c);
+putchar(' ');
+putchar(b);
+putchar(a);
+if (d == 57 && c == 56 && b == 57 && a == 57)
+break;
+putchar(',');
+putchar(' ');
+}
+a++;
+}
+b++;
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
