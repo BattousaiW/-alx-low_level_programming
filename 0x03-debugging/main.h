6118b9e14197154fd3e317d75ae13d65aaab45ec
BattousaiW <Brandonwee08@gmail.com>
2023-02-24 13:51:24 +0200

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,15 +13,15 @@ void positive_or_negative(int i)
 {
 if (i % 2 == 0)
 {
-printf("%d is positive\n", i);
+printf("%d is positive", i);
 }
 else if (i == 0)
 {
-printf("%d is zero\n", i);
+printf("%d is zero", i);
 }
 else
 {
-printf("%d is negative\n", i);
+printf("%d is negative", i);
 }
 
 #endif /* MAIN_H */
