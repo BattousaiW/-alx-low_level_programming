205e841dab736b510333f1b5fda357b55325d186
BattousaiW <Brandonwee08@gmail.com>
2023-02-24 13:44:36 +0200

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,16 +2,22 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+/**
+*positive_or_negative(int i)- checks whether number is positive
+* @i: is an integer
+* Return : Return void
+**/
 
 void positive_or_negative(int i)
 {
-if (i%2 == 0)
+if (i % 2 == 0)
 {
-printf("%d is positive\n",i);
+printf("%d is positive\n", i);
 }
 else
 {
-printf("%d is negative\n",i);
+printf("%d is negative\n", i);
 }
 }
 
