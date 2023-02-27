6b35dc42474b151f3ed67f19ed170c9406526415
BattousaiW <Brandonwee08@gmail.com>
2023-02-27 12:01:46 +0200

    more functions and loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,14 +15,12 @@ for (c = 0; c < 10; c++)
 {
 for (b = 0; b < 15; b++)
 {
-if(b >= 10)
+if (b >= 10)
 {
-_putchar((b/10) + a);
+_putchar((b / 10) + a);
 }
-else
-{
-_putchar((b%10) + 48);
-}
+
+_putchar((b % 10) + 48);
 }
 _putchar('\n');
 }
