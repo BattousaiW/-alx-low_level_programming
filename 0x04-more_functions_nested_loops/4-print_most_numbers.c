96e1251184b2fee31fcd90009260f443f7409b34
BattousaiW <Brandonwee08@gmail.com>
2023-02-27 11:38:38 +0200

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9
+ * Return: void return
+ */
+
+void print_most_numbers(void)
+{
+int a, b;
+
+a = 48;
+b = 58;
+
+while (a < b)
+{
+if (a == 2 || a == 4)
+{
+a++;
+}
+else
+{
+_putchar(a);
+a++;
+}
+}
+_putchar('\n');
+}
