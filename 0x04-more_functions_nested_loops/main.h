7d4a00cb30965acc0b969840863f53a820b29431
BattousaiW <Brandonwee08@gmail.com>
2023-02-27 18:36:50 +0200

    more functions and loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,11 +13,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-<<<<<<< HEAD
 void print_square(int size);
 void print_diagonal(int n);
-=======
-void print_diagonal(int n);
-
->>>>>>> 17f93935ad10854ff3031c8ece61e670bba5736d
 #endif
