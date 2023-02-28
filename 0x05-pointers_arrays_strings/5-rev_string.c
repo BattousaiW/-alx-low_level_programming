5a95961a1a0e5a913630d565290f871edb35d7c4
BattousaiW <Brandonwee08@gmail.com>
2023-02-28 12:24:06 +0200

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* rev_string - print a string in reverse
+* @s : stores a string 
+* Return : void.
+*/
+
+void rev_string(char *s)
+{
+int len, i;
+len = _strlen(s);
+char str[len];
+for (i = len; i > 0; i--)
+{
+str[i] = s[i];
+}
+_putchar(str);
+putchar('\n');
+
+}
