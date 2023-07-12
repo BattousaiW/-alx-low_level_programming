01b6e1847fa4df8eafca91fc7b083c0088f7bc22
Brandon Wee <Brandonwee08@gmail.com>
2023-07-12 13:06:52 +0200

    malloc free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - initializes an array of specified size
+ * @c: char to inititialize array
+ * @size: the size of the memory to print
+ *
+ * Return: array is returned when successful.
+ * NULL is returned if failed
+ */
+char *create_array(unsigned int size, char c)
+{
+char *array = (char *)malloc(size * sizeof(char));
+if (array == NULL)
+{
+return (NULL);
+}
+for (unsigned int i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - initializes an array of specified size
+ * @c: char to inititialize array
+ * @size: the size of the memory to print
+ *
+ * Return: array is returned when successful.
+ * NULL is returned if failed
+ */
