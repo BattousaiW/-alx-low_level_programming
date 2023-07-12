1128a5a14f3f70c5b5908b5c1c7ea264c6398297
Brandon Wee <Brandonwee08@gmail.com>
2023-07-12 13:39:44 +0200

    malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *array = (char *)malloc(size * sizeof(char));
-if (size == 0)
+if (array == NULL)
 {
-return (NULL);
+return (-1);
 }
 for (i = 0; i < size; i++)
 {
