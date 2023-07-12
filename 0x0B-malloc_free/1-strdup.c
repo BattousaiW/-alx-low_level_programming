79d9d5114059aa93b7602766562564cc35efab86
Brandon Wee <Brandonwee08@gmail.com>
2023-07-12 15:22:30 +0200

    mollac free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
- * create_array - initializes an array of specified size
- * @c: char to inititialize array
- * @size: the size of the memory to print
- *
- * Return: array is returned when successful.
- * NULL is returned if failed
- */
+*_strdup - Returns a pointer to a newly-allocated memory space
+*@str: char string to be copied
+*Return: a pointer to a copy of the string
+*        else NULL is returned
+*/
+
+char *_strdup(char *str)
+{
+char *res;
+int len, i;
+len = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i]; i++)
+{
+len++;
+}
+res = malloc((len + 1) * sizeof(char));
+if (res == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; str[i]; i++)
+{
+res[i] = str[i];
+}
+
+res[len] = '\0';
+
+return (res);
+}
