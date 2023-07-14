82378940bb89334ad15efc7568ba2684f3bef4dd
Brandon Wee <Brandonwee08@gmail.com>
2023-07-14 14:35:17 +0200

    more malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,14 +7,14 @@
  * @b: an unsigned integer value
  * Return: if malloc fails returns a value of 98
  * else returns success
-*/ 
+*/
 
 void *malloc_checked(unsigned int b)
 {
 void *p = malloc(b);
 if (p == NULL)
 {
-exit (98);
+exit(98);
 }
 return (p);
 }
