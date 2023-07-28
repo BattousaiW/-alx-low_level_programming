80bec00d5046e4b33acbca49222cadc19e49d259
Brandon Wee <Brandonwee08@gmail.com>
2023-07-28 12:08:17 +0200

    more-malloc updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,37 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
-*_realloc- a function that reallocates a memory block using malloc and free
-*@ptr: void pointer variable
-*@old_size: previous array size
-*@new_size: new array size
-*Return: if failed returns null
-*else returns pointer to new memory size
-*/
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *new_ptr;
-if (ptr == NULL && new_size != 0)
-{
-return (malloc(new_size));
-}
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
-}
+void *mem;
+char *ptr_copy, *filler;
+unsigned int index;
+
 if (new_size == old_size)
-{
 return (ptr);
-}
-new_ptr = malloc(new_size);
-if (new_ptr == NULL)
+
+if (ptr == NULL)
 {
+mem = malloc(new_size);
+
+if (mem == NULL)
+return (NULL);
+
+return (mem);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
 return (NULL);
 }
-memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+
+ptr_copy = ptr;
+mem = malloc(sizeof(*ptr_copy) * new_size);
+
+if (mem == NULL)
+{
 free(ptr);
-return (new_ptr);
+return (NULL);
+}
+
+filler = mem;
+
+for (index = 0; index < old_size && index < new_size; index++)
+filler[index] = *ptr_copy++;
+
+free(ptr);
+return (mem);
 }
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,7 @@
 malloc_checked - a function that allocates memory using malloc
 
 string_nconcat - a function that concatenates two strings.
+
+_calloc - a function that concatenates two strings.
+
+_realloc- a function that reallocates a memory block using malloc and free
