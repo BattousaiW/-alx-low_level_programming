69f72bfcd80e8ab87238cc41b4eb1e4333b135fd
BattousaiW <Brandonwee08@gmail.com>
2024-03-18 16:07:53 +0200

    dynamic libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,28 +1,29 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 #include <stdlib.h>
 
-/*Fuctions prototyp*/
-
 int _putchar(char c);
-int _islowercase(int c);
+int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int isCharUpper(int c);
-int check_digit(int c);
-int getStringLength(char *s);
-void _puts(char *str);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int stringToIntConverter(char *s);
-char *appendString(char *dest, char *src);
-char *cat_str(char *dest, char *src, int n);
-char *_strncpoy(char *dest, char *src, int n);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *fillMemory(char *s, char b, unsigned int n);
-char *_memcopy(char *dest, char *src, unsigned int n);
-char *locateCharacter(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* DYNAMIC_LIB */
+
+#endif /* MAIN_H*/
 
